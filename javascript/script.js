671c96aa14fe89bfcd5740d034777ab11afdbf86
lai2-01 <lai2-01@itismeucci.com>
2022-02-21 11:21:08 +0100

    feature modifica informazioni dipendenti aggiunta

--- javascript/script.js
+++ javascript/script.js
@@ -43,7 +43,8 @@ var data = [
     }
   ];
 
-var nextId = 10006;
+let nextId = 10006;
+let id;
 
 
 $(document).ready(function() {
@@ -71,7 +72,7 @@ $(document).ready(function() {
     });
 
     $("body").on("click",".delete-employee", function(){
-      let id = $(this).parent("td").data("id");
+      id = $(this).parent("td").data("id");
       for(let i = 0; i < data.length; i++){
         if(data[i].id == id){
           data.splice(i, 1);
@@ -80,6 +81,60 @@ $(document).ready(function() {
       }
       displayEmployeeList();
     });
+
+    $("body").on("click",".edit-employee", function(){
+      id =  $(this).parent("td").data("id");
+      for(let i = 0; i < data.length; i++){
+        if(data[i].id == id){
+          var myModal = new bootstrap.Modal(document.getElementById("edit-employee"), {});
+            myModal.show();
+            $('#nome-edit').val(data[i].firstName);
+            $('#cognome-edit').val(data[i].lastName);
+
+            if(data[i].gender === "M"){
+              $('#edit-sesso-m').prop("checked", true);
+            } else {
+              $('#edit-sesso-f').prop("checked", true);
+            }
+
+            $('#data-nascita-edit').val(data[i].birthDate);
+            $('#data-assunzione-edit').val(data[i].hireDate);
+            break;
+        }
+      }
+    });
+
+        //modifica le informazioni di dipendente
+        $("#edit-employee-form").submit(function(e){
+          e.preventDefault();
+          let firstName = $("#nome-edit").val();
+          let lastName = $("#cognome-edit").val();
+          let gender = $('input[name=sesso-edit]:checked', '#edit-employee-form').val();
+          let birthDate = $("#data-nascita-edit").val();
+          let hireDate = $("#data-assunzione-edit").val();
+          
+          for(let i = 0; i < data.length; i++){
+            if(data[i].id == id){
+              console.log(firstName);
+              data[i].firstName = firstName;
+              data[i].lastName = lastName;
+              data[i].gender = gender;
+              data[i].birthDate = birthDate;
+              data[i].hireDate = hireDate;
+            }
+          }
+  
+          //ripropone la lista con i nuovi valori
+          displayEmployeeList();
+    
+          //nasconde il modal
+          $("#edit-employee").hide();    
+          //senza di questo il backdrop del modal non viene rimosso
+          $('.modal-backdrop').remove(); 
+        });
+    
+
+
 });
     
     function displayEmployeeList(){
@@ -93,7 +148,8 @@ $(document).ready(function() {
             rows += '<td>' + value.birthDate + '</td>';
             rows += '<td>' + value.hireDate + '</td>';
             rows += '<td data-id="'+value.id+'">';
-                rows += '<button class="btn btn-danger delete-employee delete-employee-button"><i class="fa-solid fa-trash-can"></i></button>';
+                rows += '<button class="btn btn-success edit-employee me-2"><i class="fa-solid fa-pen"></i></button>';
+                rows += '<button class="btn btn-danger delete-employee"><i class="fa-solid fa-trash-can"></i></button>';
                 rows += '</td>';
             rows += '</td>'; 
         });
