1958380be1466fc2f99d0a8fa93ae8df12b2cf78
lai2-01 <lai2-01@itismeucci.com>
2022-03-07 10:38:17 +0100

    aggiunto delete e put

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,11 @@
 let data = [];
+const employeeData = {id:"", firstName:"", lastName:"", gender:"", birthDate:"", hireDate:""}; //oggetto dati di un singolo impiegato
 let nextId = 10006;
 let id;
 let firstPage = 0;
 let totalPages;
 let currentPage = 0;
+const defaultPath = "http://localhost:8080";
 
 $(document).ready(function () {
 
@@ -18,16 +20,6 @@ $(document).ready(function () {
     let birthDate = $("#data-nascita").val();
     let hireDate = $("#data-assunzione").val();
 
-    data.push({
-      id: nextId,
-      birthDate: birthDate,
-      firstName: firstName,
-      lastName: lastName,
-      gender: gender,
-      hireDate: hireDate
-    });
-    nextId++;
-
     //ripropone la lista con i nuovi valori
     displayEmployeeList();
 
@@ -39,55 +31,27 @@ $(document).ready(function () {
 
   $("body").on("click", ".delete-employee", function () {
     id = $(this).parent("td").data("id");
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id == id) {
-        data.splice(i, 1);
-        break;
-      }
-    }
-    displayEmployeeList();
+    $.ajax({
+      method: "DELETE",
+      url: `${defaultPath}/employees/${id}`
+    })
+      .done(function (msg) {
+        getData();
+        displayEmployeeList();
+      });
   });
 
   $("body").on("click", ".edit-employee", function () {
+
     id = $(this).parent("td").data("id");
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id == id) {
-        var myModal = new bootstrap.Modal(document.getElementById("edit-employee"), {});
-        myModal.show();
-        $('#nome-edit').val(data[i].firstName);
-        $('#cognome-edit').val(data[i].lastName);
-
-        if (data[i].gender === "M") {
-          $('#edit-sesso-m').prop("checked", true);
-        } else {
-          $('#edit-sesso-f').prop("checked", true);
-        }
-
-        $('#data-nascita-edit').val(data[i].birthDate);
-        $('#data-assunzione-edit').val(data[i].hireDate);
-        break;
-      }
-    }
+    getEmployeeData(id);
   });
 
   //modifica le informazioni di dipendente
   $("#edit-employee-form").submit(function (e) {
     e.preventDefault();
-    let firstName = $("#nome-edit").val();
-    let lastName = $("#cognome-edit").val();
-    let gender = $('input[name=sesso-edit]:checked', '#edit-employee-form').val();
-    let birthDate = $("#data-nascita-edit").val();
-    let hireDate = $("#data-assunzione-edit").val();
-
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id == id) {
-        data[i].firstName = firstName;
-        data[i].lastName = lastName;
-        data[i].gender = gender;
-        data[i].birthDate = birthDate;
-        data[i].hireDate = hireDate;
-      }
-    }
+    getNewEmployeeData();
+    editEmployee(); //put request
 
     //ripropone la lista con i nuovi valori
     displayEmployeeList();
@@ -99,16 +63,16 @@ $(document).ready(function () {
   });
 
   $("body").on("click", ".page-item", function () {
-    currentPage = $(this).data('page')
+    currentPage = $(this).data('page');
     getData();
   });
 });
 
 function getData() {
   $.ajax({
-      method: "GET",
-      url: `http://localhost:8080/employees?page=${currentPage}&size=${5}`
-    })
+    method: "GET",
+    url: `${defaultPath}/employees?page=${currentPage}&size=${5}`
+  })
     .done(function (msg) {
       data = msg['_embedded']['employees'];
       nextPage = msg['_links']['next']['href'];
@@ -116,6 +80,44 @@ function getData() {
       displayEmployeeList();
       displayPagination();
     });
+}
+
+function getEmployeeData(id) {
+  $.ajax({
+    method: "GET",
+    url: `${defaultPath}/employees/${id}`
+  })
+    .done(function (msg) {
+      employeeData.id = msg.id;
+      employeeData.firstName = msg.firstName;
+      employeeData.lastName = msg.lastName;
+      employeeData.gender = msg.gender;
+      employeeData.birthDate = msg.birthDate;
+      employeeData.hireDate = msg.hireDate;
+      openModal();
+    });
+}
+
+function getNewEmployeeData(){
+  employeeData.firstName = $("#nome-edit").val();
+  employeeData.lastName = $("#cognome-edit").val();
+  employeeData.gender = $('input[name=sesso-edit]:checked', '#edit-employee-form').val();
+  employeeData.birthDate = $("#data-nascita-edit").val();
+  employeeData.hireDate = $("#data-assunzione-edit").val();
+}
+
+function editEmployee(){
+  $.ajax({
+    method: "PUT",
+    url: `${defaultPath}/employees/${employeeData.id}`,
+    contentType: "application/json",
+    dataType: 'json',
+    data: JSON.stringify(employeeData)
+    //data: "id=employeeData.id&firstName=employeeData.firstName&lastName=employeeData.lastName&gender=employeeData.gender&birthDate=employeeData.birthDate&hireDate=employeeData.hireDate"
+  })
+  .done(function (msg) {
+    location.reload();
+  });
 }
 
 function displayEmployeeList() {
@@ -137,6 +139,23 @@ function displayEmployeeList() {
   $("tbody").html(rows);
 }
 
+function openModal(){
+  console.log(employeeData.firstName);
+        var myModal = new bootstrap.Modal(document.getElementById("edit-employee"), {});
+        myModal.show();
+        $('#nome-edit').val(employeeData.firstName);
+        $('#cognome-edit').val(employeeData.lastName);
+
+        if (employeeData.gender === "M") {
+          $('#edit-sesso-m').prop("checked", true);
+        } else {
+          $('#edit-sesso-f').prop("checked", true);
+        }
+
+        $('#data-nascita-edit').val(employeeData.birthDate);
+        $('#data-assunzione-edit').val(employeeData.hireDate);
+}
+
 function displayPagination() {
   let code = '';
   let dataPage = currentPage;
@@ -145,46 +164,46 @@ function displayPagination() {
   code += '<ul class="pagination justify-content-center">';
 
   if (currentPage === 0) {
-      code += '<li class="disabled" data-page="' + (currentPage - 1) + '"> ' +
-          '<a class="page-link" href="#" aria-label="Previous">' +
-          '<span aria-hidden="true">&laquo;</span></a></li>';
+    code += '<li class="disabled" data-page="' + (currentPage - 1) + '"> ' +
+      '<a class="page-link" href="#" aria-label="Previous">' +
+      '<span aria-hidden="true">&laquo;</span></a></li>';
   } else {
-      code += '<li class="page-item" data-page="' + (currentPage - 1) + '"> ' +
-          '<a class="page-link" href="#" aria-label="Previous">' +
-          '<span aria-hidden="true">&laquo;</span></a></li>';
+    code += '<li class="page-item" data-page="' + (currentPage - 1) + '"> ' +
+      '<a class="page-link" href="#" aria-label="Previous">' +
+      '<span aria-hidden="true">&laquo;</span></a></li>';
   }
 
   if (currentPage != 0 && currentPage != 1) {
-      code += '<li class="page-item" data-page="' + firstPage + '"><a class="page-link" href="#">' + (firstPage+1) + '</a></li>'
-      if(currentPage != 2)
+    code += '<li class="page-item" data-page="' + firstPage + '"><a class="page-link" href="#">' + (firstPage + 1) + '</a></li>'
+    if (currentPage != 2)
       code += '<li class="disabled"><a class="page-link" >...</a></li>'
   }
 
-  for (let i = getStartPage(); i < currentPage+2; i++) {
-      dataPage = i;
-      if (i === currentPage) { //Per far sì che l'elemento venga segnato come active
-          code += '<li class="page-item active" data-page="' + dataPage + '"><a class="page-link" href="#">' + (dataPage + 1) + '</a></li>'
-      } else {
-          code += '<li class="page-item" data-page="' + dataPage + '"><a class="page-link" href="#">' + (dataPage + 1) + '</a></li>'
-      }
-      if(currentPage == totalPages-2){
-          break;
-      }
+  for (let i = getStartPage(); i < currentPage + 2; i++) {
+    dataPage = i;
+    if (i === currentPage) { //Per far sì che l'elemento venga segnato come active
+      code += '<li class="page-item active" data-page="' + dataPage + '"><a class="page-link" href="#">' + (dataPage + 1) + '</a></li>'
+    } else {
+      code += '<li class="page-item" data-page="' + dataPage + '"><a class="page-link" href="#">' + (dataPage + 1) + '</a></li>'
+    }
+    if (currentPage == totalPages - 2) {
+      break;
+    }
   }
 
-  if(currentPage != totalPages-2){
-      code += '<li class="disabled"><a class="page-link" >...</a></li>'
-      code += '<li class="page-item" data-page="' + (totalPages - 2) + '"><a class="page-link" href="#">' + (totalPages + 1) + '</a></li>'
+  if (currentPage != totalPages - 2) {
+    code += '<li class="disabled"><a class="page-link" >...</a></li>'
+    code += '<li class="page-item" data-page="' + (totalPages - 2) + '"><a class="page-link" href="#">' + (totalPages + 1) + '</a></li>'
   }
 
   if (currentPage === totalPages) {
-      code += '<li class="disabled" data-page="' + (currentPage + 1) + '"> ' +
-          '<a class="page-link" href="#" aria-label="Next">' +
-          '<span aria-hidden="true">&raquo</span></a></li>';
+    code += '<li class="disabled" data-page="' + (currentPage + 1) + '"> ' +
+      '<a class="page-link" href="#" aria-label="Next">' +
+      '<span aria-hidden="true">&raquo</span></a></li>';
   } else {
-      code += '<li class="page-item" data-page="' + (currentPage + 1) + '"> ' +
-          '<a class="page-link" href="#" aria-label="Next">' +
-          '<span aria-hidden="true">&raquo</span></a></li>';
+    code += '<li class="page-item" data-page="' + (currentPage + 1) + '"> ' +
+      '<a class="page-link" href="#" aria-label="Next">' +
+      '<span aria-hidden="true">&raquo</span></a></li>';
   }
   $("pagination").html(code);
 }
@@ -192,8 +211,8 @@ function displayPagination() {
 function getStartPage() {
 
   if (currentPage === 0) {
-      return currentPage;
+    return currentPage;
   } else {
-      return currentPage - 1;
+    return currentPage - 1;
   }
 }
